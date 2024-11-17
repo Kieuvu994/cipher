67a3564f4f72e2677b3eee5117fff58774807959
kieu vu <kieuvu994@gmail.com>
2024-11-17 12:33:19 +0700

    srcs/main.py

--- srcs/main.py
+++ srcs/main.py
@@ -3,13 +3,14 @@ import rail_fence
 if __name__ == '__main__':
     med = input(' Please choose cipher or decipher (c/d) : ')
     if med == 'c':
-        pl = input('Enter method 1.cesar 2.rail-fencer 3.cesar X rail-fence : ')
+        pl = input('Enter method:  1.cesar , 2.rail-fencer , 3.cesar X rail-fence : ')
         if pl == '1':
             pt = input('Enter plaintext (cesar) : ')
-            # key = int(input('Enter key (cesar) : '))
-            # print('------------------->>>>>> CipherText (cesar) :', rail_fence.cesar(pt, 15))
+            key = int(input('Enter key (cesar) : '))
+            print('------------------->>>>>> CipherText (cesar) :', rail_fence.cesar(pt, key))
         elif pl == '2':
             pt = input('Enter plaintext (rail-fence) : ')
+            key = int(input('Enter key (rail-fence) : '))
             print('------------------->>>>>> CipherText (rail-fence) :',
-                rail_fence.rail_fence(pt, 4))
+                rail_fence.rail_fence(pt, key))
     
